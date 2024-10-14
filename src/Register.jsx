00ad5be0ed02620dac13b39aea9a5b7a171ea5bd
trangltt04 <tranglttph45373@fpt.edu.vn>
2024-10-14 15:13:46 +0700

    register

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,7 +1,138 @@
 import React from "react";
+import {
+  Button,
+  Checkbox,
+  Form,
+  Input,
+  InputNumber,
+  message,
+  Skeleton,
+} from "antd";
+import TextArea from "antd/es/input/TextArea";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import { useForm } from "antd/es/form/Form";
 
 const Register = () => {
-  return <div></div>;
+  const [messageApi, contextHolder] = message.useMessage();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const [form] = useForm();
+
+  const { mutate } = useMutation({
+    mutationFn: async (user) => {
+      await axios.post(`http://localhost:3000/register`, user);
+    },
+    onSuccess() {
+      messageApi.success("Dang ky thanh cong");
+      setTimeout(() => {
+        navigate("/login");
+      }, 1000);
+    },
+  });
+
+  const onFinish = (values) => {
+    mutate(values);
+  };
+
+  return (
+    <>
+      {contextHolder}
+      <Form
+        form={form}
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your email!",
+            },
+            {
+              type: "email",
+              message: "Khong dung dinh dang email!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          label="confirmPassword"
+          name="confirmPassword"
+          rules={[
+            {
+              required: true,
+              message: "Please input your confirmPassword!",
+            },
+            ({ getFieldValue }) => {
+              return {
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error("Mat khau khong khop"));
+                },
+              };
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
 };
 
 export default Register;
